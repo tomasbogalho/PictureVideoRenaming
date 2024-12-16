acba123f683390b8950359538c6391c87540dd36
tomasbogalho <tomasbogalho98@gmail.com>
2024-12-16 23:40:12 +0000

    adding new script for name validation v3

--- validator.py
+++ validator.py
@@ -5,13 +5,14 @@ def export_file_info(directory):
     home_directory = os.path.expanduser("~")
     year_pattern = re.compile(r'\b(19|20)\d{2}\b')
     
+    year = input("Enter the year for files that do not contain a year: ")
+    
     with open("file_info.txt", "w") as file_info:
         for root, _, files in os.walk(directory):
             for filename in files:
                 if filename.lower() == "thumbs.db":
                     continue  # Skip Thumbs.db file
                 if not year_pattern.search(filename):
-                    year = input(f"Enter the year for the file '{filename}': ")
                     filename_with_year = f"{year}_{filename}"
                     file_info.write(f"File Name: {filename_with_year}, Path: {root}\n")
                 else:
